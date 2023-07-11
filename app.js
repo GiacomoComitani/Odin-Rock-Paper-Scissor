2b83341a4851a3d79f16fcd01df444a20c0520c2
GiacomoComitani <giacomo.comitani@studenti.unimi.it>
2023-07-11 09:54:50 +0200

    fixing of some errors

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ for (let i = 0; i < cards.length; i++ ){
     cards[i].addEventListener('click', handleClick);
 }
 
-let playerScore;
-let computerScore;
+let playerScore = 0;
+let computerScore = 0;
 
 function handleClick(event) {
   const player = event.target.querySelector('p').textContent;
@@ -31,13 +31,16 @@ function handleClick(event) {
 
 function updateMoves(){
     const moves = document.querySelector('.moves');
-    moves.textContent = 'Player: ${player} - Computer: ${computer}';
+    moves.textContent = `Player: ${player} - Computer: ${computer}`;
 }
-
-
-
 
 function updateScore(){
     const scoreElement = document.querySelector('.score');
-    scoreElement.textContent = 'Player: ${playerScore} - Computer: ${computerScore}';
+    scoreElement.textContent = `Player: ${playerScore} - Computer: ${computerScore}`;
+}
+
+function generateComputer(){
+    const choices = ['sasso', 'carta', 'forbice'];
+    const index = Math.floor(Math.random()* choices.lenght);
+    return choices[index];
 }
